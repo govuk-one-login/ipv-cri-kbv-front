91d3bc718c3afb38c46632fc4bbc17a458e2b49d
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2022-03-08 14:53:33 +0000

    Add done controller

--- src/app/kbv/controllers/done.js
+++ src/app/kbv/controllers/done.js
@@ -1,5 +1,44 @@
 const BaseController = require("hmpo-form-wizard").Controller;
 
-class DoneController extends BaseController {}
+const {
+  API: {
+    PATHS: { AUTHORIZATION_CODE },
+  },
+} = require("../../../lib/config");
+
+class DoneController extends BaseController {
+  async saveValues(req, res, callback) {
+    super.saveValues(req, res, async (err, next) => {
+      if (err) {
+        next(err);
+      }
+
+      // try {
+      //   const apiResponse = await req.axios.get(AUTHORIZATION_CODE, {
+      //     headers: {
+      //       sessionId: req.session.tokenId,
+      //     },
+      //   });
+      //
+      //   console.log(apiResponse);
+      //
+      //   const code = apiResponse?.data?.code?.value;
+      //
+      //   // if (!code) {
+      //   //   res.status(500);
+      //   //   return res.send("Missing authorization code");
+      //   // }
+      //
+      //   req.session.authorization_code = code;
+
+      // res.redirect("/oauth2/");
+      // } catch (e) {
+      //   callback(e);
+      // }
+
+      callback();
+    });
+  }
+}
 
 module.exports = DoneController;
