aea4f24a548f46a8580aef5bcd71d4bd01a4df0d
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2022-03-08 15:18:26 +0000

    Add initSession and redirecttoKBV to router

--- src/app/oauth2/index.js
+++ src/app/oauth2/index.js
@@ -4,11 +4,18 @@ const router = express.Router();
 
 const {
   addAuthParamsToSession,
+  initSessionWithJWT,
   redirectToCallback,
+  redirectToKBV,
   retrieveAuthorizationCode,
 } = require("./middleware");
 
-router.get("/authorize", addAuthParamsToSession, redirectToCallback);
+router.get(
+  "/authorize",
+  addAuthParamsToSession,
+  initSessionWithJWT,
+  redirectToKBV
+);
 router.post("/authorize", retrieveAuthorizationCode, redirectToCallback);
 
 module.exports = router;
