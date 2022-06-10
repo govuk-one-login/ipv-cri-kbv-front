6c7ed8fbbc21493824dd1599f6e26ea356c5a72d
Robin Harrison <merlinc@thecourtsofchaos.com>
2022-06-10 17:47:36 +0100

    Revert "Use i18n content for dynamic questions"

--- src/lib/dynamic-i18n.js
+++ src/lib/dynamic-i18n.js
@@ -1,97 +1,58 @@
 const _ = require("lodash");
 
-const debug = require("debug")("app:lib:dynamic-i18n");
-
 function arrayify(value) {
   return [].concat(value || []);
 }
 
-const singleKeyNotDynamic = function (key) {
-  return typeof key === "string" && !key.startsWith("fields.Q");
-};
-
-const multipleKeysNotDynamic = function (key) {
-  return (
-    Array.isArray(key) && !key.some((entry) => entry.startsWith("fields.Q"))
-  );
-};
-
-const getFallbackTranslationFromFields = function (key, fields) {
-  const keys = arrayify(key);
-
-  return keys.reduce((acc, value) => {
-    return acc || _.get(fields, value);
-  }, undefined);
-};
-
-const dynamicKeyTranslation = function ({
-  key,
-  options,
-  translate,
-  fallbackTranslations,
-}) {
-  const fallbackTranslation = getFallbackTranslationFromFields(
-    key,
-    fallbackTranslations
-  );
-
-  return translate(key, {
-    default: fallbackTranslation,
-    ...options,
-  });
-};
-
-const translateWrapper = function (originalTranslate, overrideTranslations) {
-  debug(overrideTranslations);
-  return function (key, options) {
-    debug(key);
-    debug(options);
-
-    if (singleKeyNotDynamic(key) || multipleKeysNotDynamic(key)) {
-      return originalTranslate(key, options);
-    }
-
-    return dynamicKeyTranslation({
-      key,
-      options,
-      translate: originalTranslate,
-      fallbackTranslations: overrideTranslations,
-    });
-  };
-};
-
-const buildFallbackTranslations = function (question) {
-  const overrideTranslations = {
-    fields: {
-      [question.questionID]: {
-        legend: question.text,
-        label: question.text,
-        hint: question.toolTip,
-        validation: {
-          default: "You need to answer the question",
-        },
-        items: question.answerFormat.answerList.reduce((acc, answer) => {
-          const answerKey = answer;
-
-          acc[answerKey] = {
-            label: `${_.capitalize(answer)}`,
-            value: answer,
-          };
-
-          return acc;
-        }, {}),
-      },
-    },
-  };
-
-  return overrideTranslations;
-};
-
 module.exports = {
-  buildFallbackTranslations,
-  dynamicKeyTranslation,
-  getFallbackTranslationFromFields,
-  multipleKeysNotDynamic,
-  singleKeyNotDynamic,
-  translateWrapper,
+  buildOverrideTranslations(question) {
+    const overrideTranslations = {
+      fields: {
+        question: {
+          content: `${question.text}`,
+          legend: `${question.text}`,
+          label: `${question.text}`,
+          hint: `${question.toolTip}`,
+          validation: {
+            default: "You need to answer the question",
+          },
+          items: question.answerFormat.answerList.reduce((acc, answer) => {
+            const answerKey = answer;
+
+            acc[answerKey] = {
+              label: `${_.capitalize(answer)}`,
+              value: answer,
+            };
+
+            return acc;
+          }, {}),
+        },
+      },
+    };
+
+    return overrideTranslations;
+  },
+
+  translateWrapper(originalTranslate, overrideTranslations) {
+    return function (key, options) {
+      if (typeof key === "string" && !key.startsWith("fields.question")) {
+        return originalTranslate(key, options);
+      }
+
+      if (
+        Array.isArray(key) &&
+        !key.some((entry) => entry.startsWith("fields.question"))
+      ) {
+        return originalTranslate(key, options);
+      }
+
+      const keys = arrayify(key);
+
+      const prop = keys.reduce((acc, value) => {
+        return acc || _.get(overrideTranslations, value);
+      }, "");
+
+      return prop ? prop : originalTranslate(key, options);
+    };
+  },
 };
