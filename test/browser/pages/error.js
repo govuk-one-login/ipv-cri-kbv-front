280ce7ba4e1ca5b446a272d93cf9e2004db09b10
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2022-09-02 16:52:30 +0100

    test: Add browser tests for switching languages
    The current code from address-front has been reworked to fit the KBV
    structure
    
    Using the unrecoverable error page to test switching translations is a
    useful constant, even between CRIs

--- test/browser/pages/error.js
+++ test/browser/pages/error.js
@@ -14,6 +14,14 @@ module.exports = class PlaywrightDevPage {
     return "Sorry, there is a problem with the service";
   }
 
+  getLocalisedSomethingWentWrongMessage(lang) {
+    if (lang.toLowerCase() === "welsh") {
+      return "Mae'n ddrwg gennym, mae problem";
+    } else {
+      return "Sorry, there is a problem with the service";
+    }
+  }
+
   isCurrentPage() {
     return this.page.url() === this.url;
   }
