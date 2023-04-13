32093fc0fe72f7965f64864af379bbf5b8246f41
Rahul Gupta <rahul.gupta@digital.cabinet-office.gov.uk>
2023-04-13 17:51:49 +0100

    Cucumber Reporting

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,15 @@
 module.exports = {
-  default: '--publish-quiet'
-}
+  default: {
+    publishQuiet: true,
+    paths: ["./test/browser/features/**/**.feature"],
+    require: [
+      "./test/browser/support/**/*.js",
+      "./test/browser/step_definitions/**/*.js",
+    ],
+    format: [
+      "progress-bar",
+      "json:reports/cucumber-report.json",
+      "html:reports/index.html",
+    ],
+  },
+};
