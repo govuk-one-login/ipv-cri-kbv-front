1b91f560fb70a3f604e6f98cef713ecd8c0c79b3
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2026-03-10 16:18:47 +0000

    OJ-3601: Upgrade common-express

--- src/app.js
+++ src/app.js
@@ -5,27 +5,26 @@ const path = require("path");
 const session = require("express-session");
 const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const DynamoDBStore = require("connect-dynamodb")(session);
-const commonExpress = require("@govuk-one-login/di-ipv-cri-common-express");
 
+const addLanguageParam = require("@govuk-one-login/frontend-language-toggle/build/cjs/language-param-setter.cjs");
+const {
+  frontendUiMiddlewareIdentityBypass,
+} = require("@govuk-one-login/frontend-ui");
+const {
+  frontendVitalSignsInitFromApp,
+} = require("@govuk-one-login/frontend-vital-signs");
+
+const commonExpress = require("@govuk-one-login/di-ipv-cri-common-express");
+const { setup } = commonExpress.bootstrap;
 const setHeaders = commonExpress.lib.headers;
 const setScenarioHeaders = commonExpress.lib.scenarioHeaders;
 const setAxiosDefaults = commonExpress.lib.axios;
-
-const { setAPIConfig, setOAuthPaths } = require("./lib/settings");
 const { setGTM, setLanguageToggle, setDeviceIntelligence } =
   commonExpress.lib.settings;
 const { getGTM, getLanguageToggle, getDeviceIntelligence } =
   commonExpress.lib.locals;
-
-const {
-  setI18n,
-} = require("@govuk-one-login/di-ipv-cri-common-express/src/lib/i18next");
-
-const addLanguageParam = require("@govuk-one-login/frontend-language-toggle/build/cjs/language-param-setter.cjs");
-
-const {
-  frontendUiMiddlewareIdentityBypass,
-} = require("@govuk-one-login/frontend-ui");
+const { setI18n } = commonExpress.lib.i18n;
+const helmetConfig = commonExpress.lib.helmet;
 
 const {
   API,
@@ -37,9 +36,7 @@ const {
   SESSION_TTL,
   OVERLOAD_PROTECTION,
 } = require("./lib/config");
-
-const { setup } =
-  require("@govuk-one-login/di-ipv-cri-common-express").bootstrap;
+const { setAPIConfig, setOAuthPaths } = require("./lib/settings");
 
 const loggerConfig = {
   console: true,
@@ -64,12 +61,6 @@ const sessionConfig = {
   cookieOptions: { maxAge: SESSION_TTL },
   ...(SESSION_TABLE_NAME && { sessionStore: dynamoDBSessionStore }),
 };
-
-const helmetConfig = require("@govuk-one-login/di-ipv-cri-common-express/src/lib/helmet");
-
-const {
-  frontendVitalSignsInitFromApp,
-} = require("@govuk-one-login/frontend-vital-signs");
 
 const { app, router } = setup({
   config: { APP_ROOT: __dirname },
